4416ab6146cc7c052004cfb2551422052633a9ea
Raoul Malm <raoul.malm@gmail.com>
2019-05-01 20:39:58 +0200

    refactor: Code

---
+++ rnn/character_level_rnn_pt.py
@@ -0,0 +1,13 @@
+import numpy as np
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+with open('anna.txt', 'r') as f:
+    text = f.read()
+
+chars = tuple(set(text))
+int2char = dict(enumerate(chars))
+char2int = {ch: i for i,ch in int2char.items()}
+encoded = np.array([char2int[ch] for ch in text])
+
