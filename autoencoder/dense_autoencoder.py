24395b5038381fa7769c98301c970d6e79aa86c1
Raoul Malm <raoul.malm@gmail.com>
2019-05-05 18:23:13 +0200

    refactor: Code

--- autoencoder/dense_autoencoder.py
+++ autoencoder/dense_autoencoder.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', validation_size=0)
+mnist = input_data.read_data_sets('data', validation_size=0)
 
 encoding_dim = 32
 image_size = mnist.train.images.shape[1]
